688ccf8d115cc5ca766b28714bd7a969d6423a0c
RA-fi <mrgilrafiuzzaman@gmail.com>
2021-07-15 08:42:49 +0600

    get

---
+++ assist/main.js
@@ -0,0 +1,11 @@
+function showPreview(){
+    var htmlcode = document.getElementById("htmlcode").value;
+    var csscode = "<style>"+document.getElementById("csscode").value +"</style>";
+    var jscode ="<scri"+"pt>"+ document.getElementById("jscode").value + "</scri"+"pt>";
+    var phpcode ="<?php\n" + document.getElementById("phpcode").value + "?>";
+
+    var frame = document.getElementById("preview-window").contentWindow.document;
+    frame.open();
+    frame.write(htmlcode+csscode+jscode+phpcode);
+    frame.close();
+}
