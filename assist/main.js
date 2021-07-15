055f334edfced6f4b5451b91a35bc6158ed6e859
Rafiuzzaman <53997509+RA-fi@users.noreply.github.com>
2021-07-15 09:15:03 +0600

    php off

--- assist/main.js
+++ assist/main.js
@@ -2,10 +2,10 @@ function showPreview(){
     var htmlcode = document.getElementById("htmlcode").value;
     var csscode = "<style>"+document.getElementById("csscode").value +"</style>";
     var jscode ="<scri"+"pt>"+ document.getElementById("jscode").value + "</scri"+"pt>";
-    var phpcode ="<?php\n" + document.getElementById("phpcode").value + "?>";
+//     var phpcode ="<?php\n" + document.getElementById("phpcode").value + "?>";
 
     var frame = document.getElementById("preview-window").contentWindow.document;
     frame.open();
-    frame.write(htmlcode+csscode+jscode+phpcode);
+    frame.write(htmlcode+csscode+jscode);
     frame.close();
 }
